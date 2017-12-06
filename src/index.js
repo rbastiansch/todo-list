55a353e7525f1f81931d33367a2d1c6fc37cdb33
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2017-12-06 21:51:22 -0200

    add hot reload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { AppContainer } from 'react-hot-loader';
 import App from './components/App';
 import todos from './reducers';
 import { addTodo, removeTodo, clearTodos } from "./actions/index";
@@ -8,12 +9,29 @@ import { addTodo, removeTodo, clearTodos } from "./actions/index";
 const store = createStore(todos);
 
 const render = () => ReactDOM.render(
-  <App values={store.getState()}
-       addTodo={prop => store.dispatch(addTodo(prop))}
-       removeTodo={prop => store.dispatch(removeTodo(prop))}
-       clearTodos={() => store.dispatch(clearTodos())}
-  />, document.getElementById('root')
+  <AppContainer>
+    <App values={store.getState()}
+         addTodo={prop => store.dispatch(addTodo(prop))}
+         removeTodo={prop => store.dispatch(removeTodo(prop))}
+         clearTodos={() => store.dispatch(clearTodos())}
+    />
+  </AppContainer>, document.getElementById('root')
 );
 
 render();
 store.subscribe(render);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const App = require('./components/App').default;
+    ReactDOM.render(
+      <AppContainer>
+        <App values={store.getState()}
+             addTodo={prop => store.dispatch(addTodo(prop))}
+             removeTodo={prop => store.dispatch(removeTodo(prop))}
+             clearTodos={() => store.dispatch(clearTodos())}
+        />
+      </AppContainer>, document.getElementById('root')
+    );
+  })
+}
